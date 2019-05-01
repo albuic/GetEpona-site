ae4730271bea0123ba05182071f076fe58927aea
Chris Catoya <christopher.catoya@gmail.com>
2019-05-01 16:20:25 -0700

    donate addresses

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,15 +24,101 @@ const Footer = (props) => (
                 Epona is on Github. Give us a Star!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  <GitHubButton href="https://github.com/GetEpona/Epona-js" data-size="large" data-show-count="true" aria-label="Star GetEpona/Epona-js on GitHub">Star</GitHubButton>
             </p>
            
-                <h2>Contribute</h2>
-                <p>
-                    We're always looking for contributors. Epona is built in React Electron, and uses IPFS and Textile. 
-                    &nbsp;<a href="https://gateway.textile.cafe/ipfs/QmcLYowzA59kMoNAsQ5Ch8pCU3ZFxdmJjVGZD9wDjPw6Mf?key=d1x3adUwxMay1FY8kgq7hjT2x9YGtEMKJFP5SbkzBXh9dYTYKtuJGNUXoQha">Become a contributor</a>.
-                </p>
-                <h2> Feature Requests</h2>
-                    <p>Suggest new features and report any bugs on our <a href="https://github.com/GetEpona/Epona-js/issues">GitHub Issues</a> page.</p>
-                <h2>Donate</h2> 
-                <p>Setting up wallets for your shitcoins. Coming soon!</p>  
+            <h2>Contribute</h2>
+            <p>
+                We're always looking for contributors. Epona is built in React Electron, and uses IPFS and Textile. 
+                &nbsp;<a href="https://gateway.textile.cafe/ipfs/QmcLYowzA59kMoNAsQ5Ch8pCU3ZFxdmJjVGZD9wDjPw6Mf?key=d1x3adUwxMay1FY8kgq7hjT2x9YGtEMKJFP5SbkzBXh9dYTYKtuJGNUXoQha">Become a contributor</a>.
+            </p>
+            <h2> Feature Requests</h2>
+            <p>Suggest new features and report any bugs on our <a href="https://github.com/GetEpona/Epona-js/issues">GitHub Issues</a> page.</p>
+            <h2>Donate</h2> 
+            <p>
+                Leave us some Bitcoin, alts, even your shitcoins! Drop us a line in our Discord if you don't see a wallet of your preffered choice.
+            </p> 
+            <ul>
+                <li>
+                    <div>
+                        BTC
+                    </div>
+                    <input class="wallet-id" type="text" readOnly="" value="1LrufiuFTzn2ZzKsgdPH1K1AKZDenCeYrQ"/>
+                    {/* <button class="copy-button" type="button"></button>
+                    <div>
+                        class="notify-popup" style={{position: "absolute"};will-change:transorm;top:0px;left:0px;transform:translate3d(749px,999px,0px);" data-placement="top"}
+                        Copied
+                        <div class="arrow" style="left:24px;">
+                                ::before
+                        </div>
+                    </div> */}
+                </li>
+                <li>
+                    <div>
+                            BCH
+                    </div>
+                    <input class="wallet-id" type="text" readOnly="" value="192PJCrtvP7UPFpX9ZCcti2mVD6PViw3o2"/>                    
+                </li>
+                <li>
+                    <div>
+                        ETH
+                    </div>
+                    <input class="wallet-id" type="text" readOnly="" value="0x75d60297fc0b42492a3d99700db770b547c74617"/>                        
+                </li>
+                <li>
+                    <div>
+                        ETC
+                    </div>
+                    <input class="wallet-id" type="text" readOnly="" value="0x397cf3b5f19110bda7ecdfb1c7134770fdbdc487"/>                                            
+                </li>
+                <li>
+                    <div>
+                        LTC
+                    </div>
+                    <input class="wallet-id" type="text" readOnly="" value="LQqKv8mps5nP2DMHdThAnvJtNSdcSc3XJe"/>                                                                
+                </li>
+                <li>
+                    <div>
+                        NEO
+                    </div>
+                    <input class="wallet-id" type="text" readOnly="" value="AKxoCE8YjP7jyQcDCxEXHuFvtWh74Km64g"/>                                                                     
+                </li>
+                <li>
+                    <div>
+                        DOGE
+                    </div>
+                    <input class="wallet-id" type="text" readOnly="" value="DG7zCPmQYBue9rL1iHtugignX3cfJ2hb4c"/>                                                                     
+                </li>
+                <li>
+                    <div>
+                        Cardano ADA
+                    </div>
+                    <input class="wallet-id" type="text" readOnly="" value="DdzFFzCqrht2CsRqSYPvR9Mw3L2sPMgGx44CHDjLxSuQzrnw628AXiB42uiGJ1sPxTKW6352ZGBS9RG3RtzeDF6wWbTu2n6jEvF9CMdZ"/>                                                                     
+                </li>
+                <li>
+                    <div>
+                        Augur REP
+                    </div>
+                    <input class="wallet-id" type="text" readOnly="" value="0x75d60297fc0b42492a3d99700db770b547c74617"/>                                                                     
+                    
+                </li>
+                <li>
+                    <div>
+                        Lisk
+                    </div>
+                    <input class="wallet-id" type="text" readOnly="" value="12320849510539754337L"/>                                                                     
+                </li>
+                <li>
+                    <div>
+                        TRON TRX
+                    </div>
+                    <input class="wallet-id" type="text" readOnly="" value="TB3QGqV5qEPFxoN6gM8iFj3VoM19UiyMTc"/>                                                                     
+                </li>
+                <li>
+                    <div>
+                        BAT
+                    </div>
+                    <input class="wallet-id" type="text" readOnly="" value="0x75d60297fc0b42492a3d99700db770b547c74617"/>                                                                     
+                </li>
+            </ul>             
+                     
         </div>
            
         <div class="column">
@@ -42,7 +128,7 @@ const Footer = (props) => (
                 <li><img src={irc} alt="IRC" width= "24" height="24"/> <a href="irc://freenode/epona">IRC</a></li>
                 <li><img src={discord} alt="Discord" width= "24" height="24"/> <a href="https://discord.gg/DrPFqa2">Discord</a></li>
             </ul>
-             <DiscordBot/> 
+             <DiscordBot/>  
 
         </div>
     </div>
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Layout from '../components/layout'
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  
+   <Layout>
+     <h1>NOT FOUND</h1>
+     <p>You found a place that doesn&#39;t exist... yet and may never.</p>
+   </Layout>
 )
 
 export default NotFoundPage
